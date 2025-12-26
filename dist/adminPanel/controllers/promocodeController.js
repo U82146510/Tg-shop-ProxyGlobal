549a1c233730b734d6fbaa2c17bd4ea70d78fe47
U82146510 <diaconu.sandu@protonmail.com>
2025-12-26 19:10:00 +0000

    done

--- dist/adminPanel/controllers/promocodeController.js
+++ dist/adminPanel/controllers/promocodeController.js
@@ -2,7 +2,7 @@ import {Promocode} from '../../models/promoCode.js';
 
 export const getPromocode = (req,res,next)=>{
     try {
-        res.status(200).render('promocode',{error:null,message:null});
+        res.status(200).render('promocode',{error:null,message:null,promocodes:[]});
     } catch (error) {
         next(error)
     }
@@ -12,28 +12,41 @@ export const deletePromocode = async (req,res,next)=>{
     const {promoCodeName} = req.body;
     try {
         if(!promoCodeName || typeof promoCodeName !== 'string'){
-            res.status(400).render('promocode',{error:'Promocode name is required',message:null})
+            res.status(400).render('promocode',{error:'Promocode name is required',message:null,promocodes:[]})
             return;
         }
 
-        const isTherePromocode = await Promocode.findOneAndDelete({promoCodeName:promoCodeName.toLowerCase()});
+        const isTherePromocode = await Promocode.findOneAndDelete({promoCodeName:promoCodeName.toLowerCase().trim()});
         if(!isTherePromocode){
-            res.status(400).render('promocode',{error:'There is nothing to be deleted',message:null})
+            res.status(400).render('promocode',{error:'There is nothing to be deleted',message:null,promocodes:[]})
             return;
         }
 
-        res.status(200).render('promocode',{error:null,message:`Promocode ${promoCodeName} deleted.`});
+        res.status(200).render('promocode',{error:null,message:`Promocode ${promoCodeName} deleted.`,promocodes:[]});
     } catch (error) {
         next(error)
     }
 };
+
+export const getAllPromos = async(req,res,next)=>{
+    try {
+        const getAllPromocodes = await Promocode.find();
+        if(getAllPromocodes.length === 0){
+            res.status(400).render('promocode',{error:'There is no promocodes',message:null,promocodes:[]});
+            return;
+        }
+        res.status(200).render('promocode',{error:null,message:null,promocodes:getAllPromocodes});
+    } catch (error) {
+        next(error);
+    }
+}
 
 export const postPromocode = async(req,res,next)=>{
     const {promoCodeName,numberOfUse,discount,numberOfDays} = req.body;
 
     try {
         if(!promoCodeName||!numberOfUse||!discount||!numberOfDays){
-            res.status(400).render('promocode',{error:'Please fill all the fields',message:null})
+            res.status(400).render('promocode',{error:'Please fill all the fields',message:null,promocodes:[]})
             return;
         }
 
@@ -41,7 +54,8 @@ export const postPromocode = async(req,res,next)=>{
         if(Number.isNaN(days) || days <= 0 ){
             res.status(400).render('promocode', {
                 error: 'Invalid number of days',
-                message: null
+                message: null,
+                promocodes:[]
             });
             return;
         }
@@ -50,7 +64,8 @@ export const postPromocode = async(req,res,next)=>{
         if(Number.isNaN(validNumberOfUse) || validNumberOfUse <= 0){
             res.status(400).render('promocode', {
                 error: 'Invalid number of uses',
-                message: null
+                message: null,
+                promocodes:[]
             });
             return;
         }
@@ -59,7 +74,8 @@ export const postPromocode = async(req,res,next)=>{
         if(Number.isNaN(validDiscount) || validDiscount <= 0){
             res.status(400).render('promocode', {
                 error: 'Invalid number for discount',
-                message: null
+                message: null,
+                promocodes:[]
             });
             return;
         }
@@ -68,15 +84,15 @@ export const postPromocode = async(req,res,next)=>{
         const millisecondsInDay = 24*60*60*1000;
         const expire = new Date(currentDate.getTime()+(millisecondsInDay * days));
 
-        let isTherePromocode = await Promocode.findOne({promoCodeName:promoCodeName.toLowerCase()});
+        let isTherePromocode = await Promocode.findOne({promoCodeName:promoCodeName.toLowerCase().trim()});
         if(!isTherePromocode){
             isTherePromocode = await Promocode.create({
                 promoCodeName:promoCodeName.toLowerCase() ,numberOfUse:validNumberOfUse,discount:validDiscount,users: [],expire
             });
-            res.status(201).render('promocode',{ error: null, message: 'Promocode created successfully' });
+            res.status(201).render('promocode',{ error: null, message: 'Promocode created successfully',promocodes:[] });
             return;
         }
-        res.status(200).render('promocode',{ error: "This promocode already exists", message: null });
+        res.status(200).render('promocode',{ error: "This promocode already exists", message: null,promocodes:[] });
 
     } catch (error) {
         next(error)
--- dist/adminPanel/routes/promocodeRoute.js
+++ dist/adminPanel/routes/promocodeRoute.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import {postPromocode,getPromocode,deletePromocode} from '../controllers/promocodeController.js'
+import {postPromocode,getPromocode,deletePromocode,getAllPromos} from '../controllers/promocodeController.js'
 
 export const promocodeRoute = Router();
 
 promocodeRoute.get('/promocode',getPromocode);
+promocodeRoute.post('/allpromocodes',getAllPromos);
 promocodeRoute.post('/promocode',postPromocode);
 promocodeRoute.post('/deletepromocode',deletePromocode);
